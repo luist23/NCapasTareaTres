20fe267e5a1a09184468ea13d6a8dc65588e3d80
luist23 <00127717@uca.edu.sv>
2020-04-28 20:48:45 -0600

    add validation to alumno

--- src/main/java/com/uca/capas/modelo/domain/Alumno.java
+++ src/main/java/com/uca/capas/modelo/domain/Alumno.java
@@ -2,18 +2,22 @@ package com.uca.capas.modelo.domain;
 
 import java.util.Date;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class Alumno {
 	
-	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	@Size(min=1, max=25, message = "El campo nombre debe tener una longitud entre 1 y 25 caracteres.")
+	@NotEmpty
 	String nombre;
 	
-	@NotEmpty(message = "El campo contrasenia no puede estar vacio.")
+	@Size(min=1, max=25, message = "El campo nombre debe tener una longitud entre 1 y 25 caracteres.")
+	@NotEmpty
 	String apellido;
 	
-	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	//@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	@NotEmpty
 	Date fechaN;
 	
 	@NotEmpty(message = "El campo contrasenia no puede estar vacio.")
