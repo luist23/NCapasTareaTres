0e59404ac5eaf68fbbcee9e787237302b0d6c4b8
luist23 <00127717@uca.edu.sv>
2020-04-28 20:31:37 -0600

    definiendo alumno

--- src/main/java/com/uca/capas/modelo/domain/Alumno.java
+++ src/main/java/com/uca/capas/modelo/domain/Alumno.java
@@ -1,5 +1,30 @@
 package com.uca.capas.modelo.domain;
 
-public class Alumno {
+import java.util.Date;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Alumno {
+	
+	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	String nombre;
+	
+	@NotEmpty(message = "El campo contrasenia no puede estar vacio.")
+	String apellido;
+	
+	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	Date fechaN;
+	
+	@NotEmpty(message = "El campo contrasenia no puede estar vacio.")
+	String direccionN;
+	
+	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	String Insti;
+	
+	@NotEmpty(message = "El campo contrasenia no puede estar vacio.")
+	String celular;
+	
+	@Size(min=1, max=25, message = "El campo usuario debe tener una longitud entre 1 y 25 caracteres.")
+	String telefono;
 }
